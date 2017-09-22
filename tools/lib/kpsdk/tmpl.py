a4c7694ae56bb8ed5edaa47a15f797d371621807
Jean-Charles Lefebvre <polyvertex@gmail.com>
2017-09-22 14:02:00 +0200

    tmpl lib: allow to customize tag delimiters

--- tools/lib/kpsdk/tmpl.py
+++ tools/lib/kpsdk/tmpl.py
@@ -5,46 +5,54 @@ import os
 import shutil
 from . import _utils
 
-__all__ = ["do_tag", "do_text", "do_file", "do_tree"]
+__all__ = ["do_tag", "do_text", "do_file", "do_tree", "TAG_DELIMITERS"]
 
-def do_tag(text, name, value):
+TAG_DELIMITERS = ( ('{{', '}}'), (b'{{', b'}}') )
+
+def do_tag(text, name, value, tag_delim=TAG_DELIMITERS):
     if isinstance(text, bytes):
-        tag = b'{{' + bytes(name, encoding="utf-8", errors="strict") + b'}}'
+        tag = (tag_delim[1][0] +
+               bytes(name, encoding="utf-8", errors="strict") +
+               tag_delim[1][1])
         value = bytes(value, encoding="utf-8", errors="strict")
     else:
-        tag = '{{' + name + '}}'
+        tag = tag_delim[0][0] + name + tag_delim[0][1]
 
     return text.replace(tag, value)
 
-def do_text(text, tmpl_dict):
+def do_text(text, tmpl_dict, tag_delim=TAG_DELIMITERS):
     for name, value in tmpl_dict.items():
-        text = do_tag(text, name, value)
+        text = do_tag(text, name, value, tag_delim)
     return text
 
-def do_file(src_file, dest_file, tmpl_dict):
+def do_file(src_file, dest_file, tmpl_dict, tag_delim=TAG_DELIMITERS):
     with open(src_file, mode="rb") as fin:
         with open(dest_file, mode="wb") as fout:
             # to read the entire file at once is not optimized but this allows
             # not to alter the original encoding and format (line ending)
-            fout.write(do_text(fin.read(), tmpl_dict))
+            fout.write(do_text(fin.read(), tmpl_dict, tag_delim))
 
     shutil.copystat(src_file, dest_file, follow_symlinks=False)
 
-def do_tree(root_src, root_dest, tmpl_dict, tmpl_ext=(".in", ".tmpl")):
-    _do_tree(root_src, root_dest, tmpl_dict, tmpl_ext)
+def do_tree(root_src, root_dest,
+            tmpl_dict, tmpl_ext=(".in", ".tmpl"),
+            tag_delim=TAG_DELIMITERS):
+    _do_tree(root_src, root_dest, tmpl_dict, tmpl_ext, tag_delim)
 
 
-def _do_tree(root_src, root_dest, tmpl_dict, tmpl_ext, level=0):
+def _do_tree(root_src, root_dest, tmpl_dict, tmpl_ext, tag_delim, level=0):
     if level == 0:
         _mkdir(root_dest)
 
     for entry in os.scandir(root_src):
         src_path = os.path.join(root_src, entry.name)
-        dest_path = os.path.join(root_dest, do_text(entry.name, tmpl_dict))
+        dest_path = os.path.join(root_dest,
+                                 do_text(entry.name, tmpl_dict, tag_delim))
 
         if entry.is_dir():
             _mkdir(dest_path, copy_stats_from=src_path)
-            _do_tree(src_path, dest_path, tmpl_dict, tmpl_ext, level + 1)
+            _do_tree(src_path, dest_path, tmpl_dict, tmpl_ext, tag_delim,
+                     level + 1)
         elif entry.is_file():
             was_tmpl = False
             for ext in tmpl_ext:
@@ -52,7 +60,7 @@ def _do_tree(root_src, root_dest, tmpl_dict, tmpl_ext, level=0):
                 if entry.name.lower().endswith(ext):
                     was_tmpl = True
                     dest_path = dest_path[0:-len(ext)]
-                    do_file(src_path, dest_path, tmpl_dict)
+                    do_file(src_path, dest_path, tmpl_dict, tag_delim)
                     break
 
             if not was_tmpl:
@@ -67,4 +75,5 @@ def _mkdir(new_dir, copy_stats_from=None):
         if not _utils.dir_is_empty(new_dir):
             raise RuntimeError("destination directory not empty: " + new_dir)
     else:
-        raise RuntimeError("destination exists and is not a directory: " + new_dir)
+        raise RuntimeError("destination exists and is not a directory: " +
+                           new_dir)
